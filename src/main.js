3f227efe83d84d3918797283b182aa0abdf20fff
lvsecret <13548672218@163.com>
2018-11-25 12:50:51 +0800

    完成了首页网络数据渲染 跳转到详情页 详情页数据获取 使用watch观察url改变 更新页面数据 iview Element-ui 整合 图钉 分页 返回顶部

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+//导入element-ui框架
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);//注册到Vue上
+//导入iv写完了框架
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
+//导入aios
+//类似于 vue-resource this.$http
+import axios from 'axios'
+Vue.prototype.$axios=axios;
 //导入路由
 import VueRouter from 'vue-router';
 Vue.use(VueRouter)
@@ -9,21 +21,37 @@ Vue.use(VueRouter)
 import './assets/site/css/style.css'
 //导入每一个页面的组件
 import index from './components/index.vue'
+import detail from './components/02.detail.vue'
 // 写路由规则
 let routes=[
   {
     path:'/',
     //重定向到首页/index
     redirect:'/index'
-  },{
+  },
+  {
     path:'/index',
     component:index
+  },
+  {
+    path:'/detail/:artID',
+    component:detail
   }
 ]
 // 实例化路由对象
 let router=new VueRouter({
   routes
 });
+//注册全局过滤器 方便使用
+//导入moment
+import moment from 'moment';
+Vue.filter('shortTime',value=>{
+  return moment(value).format("YYYY😘MM😘DD👍");
+});
+Vue.filter('shortTimePlus',value=>{
+  return moment(value).format("YYYY/MM/DD HH:mm:ss");
+});
+
 new Vue({
   render: h => h(App),
   //传入路由对象
